5908953db4d3b2ed58652f52af178eb1667f8621
Chris Lang <chrislang51020@gmail.com>
2023-03-19 16:08:16 -0500

    Added back button to user page to go back to home page.

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function UserPage() {
     return (
         <div>
-            <h1>Hello, User Page</h1>;
+            <h1>Hello, User Page</h1>
+            <Link to="/"><button>Go Back</button></Link>
         </div>
     )
 }
