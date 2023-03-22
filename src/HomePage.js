3711ff26bd00699f16f99e3e7076eb86c88f392a
Chris Lang <chrislang51020@gmail.com>
2023-03-21 22:24:47 -0500

    Added SVG clock component and CSS edits.

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import Clock from "./Clock";
 
 export default function HomePage() {
 
@@ -18,6 +19,7 @@ export default function HomePage() {
             <Link to="/user"><button>Go to User Page</button></Link>
             <button onClick={authenticateUser}>{authenticated ? "Log out" : "Log In"}</button>
             {authenticated && <Link to="/search"><button>Go to Search page</button></Link>}
+            <Link to="/user" ClassName="ClockLink"><Clock /></Link>
         </div>
     )
 }
