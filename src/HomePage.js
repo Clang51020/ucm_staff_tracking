c1c0a2acc7f440695f71bbf4b5eef50408e6b82d
Chris Lang <chrislang51020@gmail.com>
2023-04-16 20:53:32 -0500

    Editing some css and adding in date components.

--- src/HomePage.js
+++ src/HomePage.js
@@ -14,6 +14,9 @@ export default function HomePage() {
             setAuthentication(true);
         }
     }
+    const [date, setDate] = useState(new Date());
+    const formattedDate = date.getMonth()+1 + '/' + date.getDate() + '/' + date.getFullYear();
+    const formattedTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
     
     return (
         <>
@@ -25,8 +28,9 @@ export default function HomePage() {
             {authenticated && <Link to="/search"><button>Go to Search page</button></Link>}
             <Link to="/user" ClassName="ClockLink"><Clock /></Link>
             <HomeIcon />
-            <h1>Home Page</h1>
-            <h1>Home Page</h1>
+            <h1>{formattedDate}</h1>
+            <h1>{formattedTime}</h1>
+            <h1>Date Should Be Above This</h1>
             <h1>Home Page</h1>
             <h1>Home Page</h1>
             <h1>Home Page</h1>
